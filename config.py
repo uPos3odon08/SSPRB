ccdea56b7871166cb60653f100231b273a2cad99
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2023-01-23 17:12:14 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+stalksub=("The Subreddit You Want To Stalk The Member Count Of") 
+PostOnSub=("The Subreddit You Want To PostSomething To")
+ClientId=("Your Reddit App Client Secret")
+ClientSecret=("Your Reddit App Client Secret")
+UserName=("The Username Of The Bot Account")
+PWD=("The Password Of The Bot Account")
+UserAgent=("Bot Script By u/YourRedditUsername")
+
+BOTMSG=("The total amount of members in AT") #doesn't work atm so no need to bother with it
